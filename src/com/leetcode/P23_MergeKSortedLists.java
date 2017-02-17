829cbe9029ebc358be9c6263c24d029f38c6be61
xiaoyuan <joy32812@qq.com>
2017-02-17 14:45:51 +0800

    test

---
+++ src/com/leetcode/P23_MergeKSortedLists.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+/**
+ * Created by xiaoyuan on 16/02/2017.
+ */
+public class P23_MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
