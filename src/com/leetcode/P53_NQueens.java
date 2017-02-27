71bcfe789f341128c94a9fe57f717dab8ccdf065
xiaoyuan <joy32812@qq.com>
2017-02-27 20:04:56 +0800

    P53_NQueens

---
+++ src/com/leetcode/P53_NQueens.java
@@ -0,0 +1,65 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiaoyuan on 27/02/2017.
+ * Just search
+ */
+public class P53_NQueens {
+
+    public void dfs(List<List<String>> result, int[] pos, int ind) {
+        if (ind == pos.length) {
+            List<String> oneSolution = new ArrayList<String>();
+            for(int i = 0; i < pos.length; i++) {
+                int col = pos[i];
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < pos.length;j++) {
+                    if (j != col) {
+                        sb.append('.');
+                    } else {
+                        sb.append('Q');
+                    }
+                }
+                oneSolution.add(sb.toString());
+            }
+
+            result.add(oneSolution);
+            return;
+        }
+
+
+        for (int j = 0; j < pos.length; j++){
+            if (okay(j, pos, ind)) {
+                pos[ind] = j;
+                dfs(result, pos, ind + 1);
+            }
+        }
+    }
+
+    private boolean okay(int j, int[] pos, int ind) {
+        for (int i = 0; i < ind; i++) {
+            if (j == pos[i] || i + pos[i] == ind + j || i - pos[i] == ind - j) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        int[] pos = new int[n];
+
+        dfs(result, pos, 0);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new P53_NQueens().solveNQueens(10));
+    }
+
+}
