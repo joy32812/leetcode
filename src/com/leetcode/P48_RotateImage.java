881927e25ad56a86e0ee6f7e3551c34d531bd599
xiaoyuan <joy32812@qq.com>
2017-02-27 19:17:04 +0800

    P48_RotateImag

---
+++ src/com/leetcode/P48_RotateImage.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+/**
+ * Created by xiaoyuan on 27/02/2017.
+ */
+public class P48_RotateImage {
+
+    public void rotate(int[][] matrix) {
+        if (matrix.length == 0) {
+            return;
+        }
+        int[][] result = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                result[i][j] = matrix[matrix.length - j - 1][i];
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = result[i][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        new P48_RotateImage().rotate(matrix);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
