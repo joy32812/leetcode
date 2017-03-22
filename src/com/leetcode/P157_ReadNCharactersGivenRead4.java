a5d647c3dc4b0a73ef66b93e90b49f1c7bb5f000
xiaoyuan <joy32812@qq.com>
2017-03-22 22:13:51 +0800

    P157

--- src/com/leetcode/P157_ReadNCharactersGivenRead4.java
+++ src/com/leetcode/P157_ReadNCharactersGivenRead4.java
@@ -8,8 +8,18 @@ public class P157_ReadNCharactersGivenRead4 {
     private boolean available;
     private String last;
 
+
+    private boolean al;
+
     private int read4(char[] buf) {
-        return 0;
+        if (al == true) {return 0;}
+
+        al = true;
+        buf[0] = 'a';
+        buf[1] = 'b';
+        buf[2] = 'c';
+        buf[3] = 'd';
+        return 4;
     }
 
     public int read(char[] buf, int n) {
@@ -20,16 +30,18 @@ public class P157_ReadNCharactersGivenRead4 {
 
         if (!available) {
             if (last.length() > 0) {
-                for (int i = 0; i < last.length(); i++) {buf[i] = last.charAt(i);}
-                last = "";
-                return buf.length;
+                int cnt = Math.min(n, last.length());
+                StringBuilder sb = new StringBuilder(last.substring(0,Math.min(n, last.length())));
+                last = last.substring(Math.min(n, last.length()));
+
+                for (int i = 0; i < sb.length(); i++) {buf[i] = sb.charAt(i);}
+                return cnt;
             }
             return 0;
         }
 
         int cnt = Math.min(n, last.length());
-        StringBuilder sb = new StringBuilder(last.substring(0,Math.min(n, last.length())));
-        last = last.substring(Math.min(n, last.length()));
+        StringBuilder sb = new StringBuilder(last);
 
         char[] buffer = new char[4];
         while (cnt < n) {
@@ -42,11 +54,46 @@ public class P157_ReadNCharactersGivenRead4 {
             }
         }
 
-        for (int i = 0; i < cnt; i++) {
+        for (int i = 0; i < Math.min(cnt, n); i++) {
             buf[i] = sb.charAt(i);
         }
+
         last = sb.substring(Math.min(n, cnt));
         return Math.min(n, cnt);
+    }
+
+    public static void main(String[] args) {
+        P157_ReadNCharactersGivenRead4 obj = new P157_ReadNCharactersGivenRead4();
+
+        char[] buf = new char[1];
+        int num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
+
+        buf = new char[1];
+        num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
+
+        buf = new char[1];
+        num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
+
+        buf = new char[1];
+        num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
+
+        buf = new char[1];
+        num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
+
+        buf = new char[1];
+        num = obj.read(buf, 1);
+        System.out.println(num);
+        System.out.println(buf[0]);
     }
 
 }
