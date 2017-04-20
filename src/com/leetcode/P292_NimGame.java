e70a3f22292f8ffff846c6fa978e1e6b1bb0bb27
joy32812 <joy32812@qq.com>
2017-04-21 00:30:28 +0800

    easy game

---
+++ src/com/leetcode/P292_NimGame.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+/**
+ * Created by xiaoyuan on 21/04/2017.
+ */
+public class P292_NimGame {
+
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {return false;}
+        return true;
+    }
+
+}
