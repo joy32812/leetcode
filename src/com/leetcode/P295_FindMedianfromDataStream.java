cb0de7e2dd7143628b43a17f6959c345b3ab2905
xiaoyuan <joy32812@qq.com>
2017-04-21 20:43:03 +0800

    P295 finish later

---
+++ src/com/leetcode/P295_FindMedianfromDataStream.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by xiaoyuan on 21/04/2017.
+ */
+public class P295_FindMedianfromDataStream {
+
+
+    /** initialize your data structure here. */
+    public P295_FindMedianfromDataStream() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        TreeMap<String, String> treeMap = new TreeMap<>();
+        treeMap.put("11", "22");
+        treeMap.put("11", "22");
+
+
+
+        System.out.println(treeMap.size());
+    }
+
+}
