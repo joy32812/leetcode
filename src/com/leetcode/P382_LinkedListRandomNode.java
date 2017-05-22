0a5ba91b4f1b5f9ba2869577799121c3db568850
xiaoyuan <joy32812@qq.com>
2017-05-22 21:10:00 +0800

    P382 one pass for the list

---
+++ src/com/leetcode/P382_LinkedListRandomNode.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import java.util.Random;
+
+/**
+ * Created by xiaoyuan on 22/05/2017.
+ */
+public class P382_LinkedListRandomNode {
+
+
+}
+
+class Solution {
+
+    private ListNode h;
+    private Random random;
+    /** @param head The linked list's head.
+    Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        h = head;
+        random = new Random();
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+
+        ListNode head = h;
+        int result = head.val;
+        for (int i = 1; head != null; i++) {
+            if (random.nextInt(i) == 0) {
+                result = head.val;
+            }
+
+            head = head.next;
+        }
+
+        return result;
+    }
+}
