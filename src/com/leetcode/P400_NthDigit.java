334795ced3f24e12a10b53bd683bb3200916fe4d
xiaoyuan <joy32812@qq.com>
2017-05-27 18:35:33 +0800

    P400

---
+++ src/com/leetcode/P400_NthDigit.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by xiaoyuan on 27/05/2017.
+ */
+public class P400_NthDigit {
+
+    public int findNthDigit(int n) {
+
+        long tens = 1;
+        long total = 0;
+        int i = 1;
+        int left = n;
+        for (; ;i++) {
+            total += 9 * tens;
+            if (total >= n) {break;}
+            left -= 9 * tens;
+            tens *= 10;
+        }
+
+        System.out.println(i + "  " + left + "   " + tens);
+        left--;
+
+        long a = tens + (left / i);
+        long b = left % i;
+        System.out.println("---->  " + a + "   " + b);
+
+        List<Long> list = new ArrayList<>();
+        while (a > 0) {
+            list.add(a % 10);
+            a /= 10;
+        }
+        Collections.reverse(list);
+
+        return list.get((int)(b)).intValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new P400_NthDigit().findNthDigit(99));
+        System.out.println(new P400_NthDigit().findNthDigit(100));
+        System.out.println(new P400_NthDigit().findNthDigit(101));
+    }
+
+}
