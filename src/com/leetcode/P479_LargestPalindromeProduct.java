402342a7c3ee5080efdb08cc51eee8899e3baaa3
xiaoyuan <joy32812@qq.com>
2017-07-30 13:28:08 +0800

    P479 greedy

--- src/com/leetcode/P479_LargestPalindromeProduct.java
+++ src/com/leetcode/P479_LargestPalindromeProduct.java
@@ -5,12 +5,29 @@ package com.leetcode;
  */
 public class P479_LargestPalindromeProduct {
 
-    public int largestPalindrome(int n) {
+    public int largestPalindrome(long n) {
+        if (n == 1) {return 9;}
+
+        int max = (int)Math.pow(10, n) - 1;
+
+        for (int i = max - 1; i > max / 10; i--) {
+            long u = Long.valueOf(i + new StringBuilder().append(i).reverse().toString());
+
+            for (long x = max; x * x >= u; x--) {
+                if (u % x == 0) {
+                    return (int)(u % 1337);
+                }
+            }
+        }
+
         return 0;
     }
 
+
     public static void main(String[] args) {
-
+        System.out.println(new P479_LargestPalindromeProduct().largestPalindrome(2));
+        System.out.println(new P479_LargestPalindromeProduct().largestPalindrome(8));
+        System.out.println(new P479_LargestPalindromeProduct().largestPalindrome(7));
+        System.out.println(new P479_LargestPalindromeProduct().largestPalindrome(6));
     }
-
 }
