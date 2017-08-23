e7ef113015a8eb86786aa297c930e688db362804
xiaoyuan <joy32812@qq.com>
2017-08-23 18:22:15 +0800

    P324 wiggle sort

--- src/com/leetcode/P324_WiggleSortII.java
+++ src/com/leetcode/P324_WiggleSortII.java
@@ -1,5 +1,6 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +10,20 @@ import java.util.Map;
 public class P324_WiggleSortII {
 
     public void wiggleSort(int[] nums) {
+
+        int n = nums.length;
+        int[] sorted = new int[n];
+        for (int i = 0; i < n; i++) {sorted[i] = nums[i];}
+        Arrays.sort(sorted);
+
+        int cnt = 0;
+        for (int i = (n - 1) % 2 == 0 ? n - 1 : n - 2; i >= 0; i -= 2) {
+            nums[i] = sorted[cnt++];
+        }
+
+        for (int i = (n - 1) % 2 == 0 ? n - 2 : n - 1; i >= 0; i -= 2) {
+            nums[i] = sorted[cnt++];
+        }
     }
 
 }
