d74f645ff5e26107a0a6df80a9607c5fd0e9bb73
xiaoyuan <joy32812@qq.com>
2019-04-25 23:10:37 +0800

    P562 dp

--- src/com/leetcode/P562_LongestLineofConsecutiveOneinMatrix.java
+++ src/com/leetcode/P562_LongestLineofConsecutiveOneinMatrix.java
@@ -6,47 +6,28 @@ package com.leetcode;
 public class P562_LongestLineofConsecutiveOneinMatrix {
 
     public int longestLine(int[][] M) {
-        if (M == null || M.length == 0 || M[0].length == 0) {return 0;}
-
-        int ans = getMax(M, 0, 1);
-        ans = Math.max(ans, getMax(M, 1, 0));
-        ans = Math.max(ans, getMax(M, 1, 1));
-        ans = Math.max(ans, getMax(M, -1, 1));
-
-        return ans;
-    }
-
-    private int getMax(int[][] M, int dx, int dy) {
-        int m = M.length;
-        int n = M[0].length;
+        if (M == null || M.length == 0) return 0;
 
         int ans = 0;
+
+        int m = M.length;
+        int n = M[0].length;
+        int[][][] dp = new int[m][n][4];
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (M[i][j] == 0) {continue;}
-                if (i - dx < m && i - dx >= 0 && j - dy < n && j - dy >= 0 && M[i - dx][j - dy] == 1) {continue;}
+                if (M[i][j] == 0) continue;
+                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = dp[i][j][3] = 1;
 
-                ans = Math.max(ans, getLen(M, i, j, dx, dy));
+                if (j - 1 >= 0) dp[i][j][0] += dp[i][j - 1][0];
+                if (i - 1 >= 0) dp[i][j][1] += dp[i - 1][j][1];
+                if (i - 1 >= 0 && j - 1 >= 0) dp[i][j][2] += dp[i - 1][j - 1][2];
+                if (i - 1 >= 0 && j + 1 < n) dp[i][j][3] += dp[i - 1][j + 1][3];
+
+                for (int k = 0; k < 4; k++) ans = Math.max(ans, dp[i][j][k]);
             }
         }
 
-        return ans;
-    }
-
-
-    private int getLen(int[][] M, int x, int y, int dx, int dy) {
-
-        int m = M.length;
-        int n = M[0].length;
-
-        int ans = 1;
-        // diagonal
-        int len = 1;
-        for (int i = x + dx, j = y + dy; i >=0 && i < m && j >= 0 && j < n; i += dx, j += dy) {
-            if (M[i][j] == 0) {break;}
-            len++;
-        }
-        ans = Math.max(ans, len);
 
         return ans;
     }
