d0ff0e31d3439a19df197da85e12a0b81e63e8c7
xiaoyuan <joy32812@qq.com>
2019-04-25 12:38:21 +0800

    P96

--- src/com/leetcode/P505_TheMazeII.java
+++ src/com/leetcode/P505_TheMazeII.java
@@ -1,185 +1,73 @@
 package com.leetcode;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by xiaoyuan on 24/06/2017.
  */
 public class P505_TheMazeII {
 
-    int[][] leftWalls;
-    int[][] rightWalls;
-    int[][] upWalls;
-    int[][] downWalls;
-    int[][] res;
-    int m, n;
+
+    int[] dx = {0, 0, 1, -1};
+    int[] dy = {1, -1, 0, 0};
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        if (maze == null || maze.length == 0) return -1;
 
-        m = maze.length;
-        n = maze[0].length;
 
-        leftWalls = new int[m][n];
-        rightWalls = new int[m][n];
-        upWalls = new int[m][n];
-        downWalls = new int[m][n];
-        res = new int[m][n];
+        int m = maze.length;
+        int n = maze[0].length;
 
-        getLeftWalls(maze);
-        getRightWalls(maze);
-        getUpWalls(maze);
-        getDownWalls(maze);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                res[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        res[start[0]][start[1]] = 0;
-
-        bfs(maze, start[0], start[1]);
-
-        return res[destination[0]][destination[1]] != Integer.MAX_VALUE ? res[destination[0]][destination[1]] : -1;
-    }
-
-    private void bfs(int[][] maze, int x, int y) {
 
         Queue<Integer> Q = new LinkedList<>();
-        Set<Integer> inQ = new HashSet<>();
+        Q.add(start[0] * n + start[1]);
 
-        Q.add(x * n + y);
-        inQ.add(x * n + y);
+        Set<Integer> inQ = new HashSet<>();
+        inQ.add(Q.peek());
+
+        Map<Integer, Integer> disMap = new HashMap<>();
+        disMap.put(Q.peek(), 0);
 
 
         while (!Q.isEmpty()) {
-            int tp = Q.poll();
-            inQ.remove(tp);
+            int now = Q.poll();
+            int x = now / n;
+            int y = now % n;
 
-            int nx = tp / n;
-            int ny = tp % n;
+            inQ.remove(now);
 
-            int tx = nx;
-            int ty = ny;
-            //left
-            tx = nx;
-            ty = leftWalls[nx][ny] + 1;
+            for (int k = 0; k < dx.length; k ++) {
 
-            int len = Math.abs(ty - ny);
-            if (tx >= 0 && tx < m && ty >= 0 && ty < n && res[tx][ty] > res[nx][ny] + len) {
-                res[tx][ty] = res[nx][ny] + len;
+                int tx = x + dx[k];
+                int ty = y + dy[k];
+                int cnt = 0;
 
-                int key = tx * n + ty;
-                if (!inQ.contains(key)) {
-                    Q.add(key);
-                    inQ.add(key);
+                while (tx >= 0 && tx < m && ty >= 0 && ty < n && maze[tx][ty] == 0) {
+                    cnt ++;
+                    tx += dx[k];
+                    ty += dy[k];
                 }
-            }
+
+                tx -= dx[k];
+                ty -= dy[k];
 
 
-            //right
-            tx = nx;
-            ty = rightWalls[nx][ny] - 1;
-            len = Math.abs(ty - ny);
-            if (tx >= 0 && tx < m && ty >= 0 && ty < n && res[tx][ty] > res[nx][ny] + len) {
-                res[tx][ty] = res[nx][ny] + len;
+                int val = tx * n + ty;
 
-                int key = tx * n + ty;
-                if (!inQ.contains(key)) {
-                    Q.add(key);
-                    inQ.add(key);
-                }
-            }
-
-
-            //up
-            ty = ny;
-            tx = upWalls[nx][ny] + 1;
-            len = Math.abs(tx - nx);
-            if (tx >= 0 && tx < m && ty >= 0 && ty < n && res[tx][ty] > res[nx][ny] + len) {
-                res[tx][ty] = res[nx][ny] + len;
-
-                int key = tx * n + ty;
-                if (!inQ.contains(key)) {
-                    Q.add(key);
-                    inQ.add(key);
-                }
-            }
-
-            //down
-            ty = ny;
-            tx = downWalls[nx][ny] - 1;
-            len = Math.abs(tx - nx);
-            if (tx >= 0 && tx < m && ty >= 0 && ty < n && res[tx][ty] > res[nx][ny] + len) {
-                res[tx][ty] = res[nx][ny] + len;
-
-                int key = tx * n + ty;
-                if (!inQ.contains(key)) {
-                    Q.add(key);
-                    inQ.add(key);
-                }
-            }
-
-        }
-
-    }
-
-    private void getDownWalls(int[][] maze) {
-        for (int j = 0; j < n; j++) {
-            int wall = m;
-
-            for (int i = m - 1; i >= 0; i--) {
-                if (maze[i][j] == 1) {
-                    wall = i;
-                } else {
-                    downWalls[i][j] = wall;
-                }
-            }
-
-        }
-    }
-
-    private void getUpWalls(int[][] maze) {
-        for (int j = 0; j < n; j++) {
-            int wall = -1;
-
-            for (int i = 0; i < m; i++) {
-                if (maze[i][j] == 1) {
-                    wall = i;
-                } else {
-                    upWalls[i][j] = wall;
-                }
-            }
-
-        }
-    }
-
-    private void getRightWalls(int[][] maze) {
-        for (int i = 0; i < m; i++) {
-            int wall = n;
-            for (int j = n - 1; j >= 0; j--) {
-                if (maze[i][j] == 1) {
-                    wall = j;
-                } else {
-                    rightWalls[i][j] = wall;
+                if (disMap.getOrDefault(val, Integer.MAX_VALUE) > disMap.get(now) + cnt) {
+                    disMap.put(val, disMap.getOrDefault(now, 0) + cnt);
+                    if (!inQ.contains(val)) {
+                        inQ.add(val);
+                        Q.add(val);
+                    }
                 }
             }
         }
+
+        int dest = destination[0] * n + destination[1];
+
+        return disMap.getOrDefault(dest, -1);
     }
 
-    private void getLeftWalls(int[][] maze) {
-        for (int i = 0; i < m; i++) {
-            int wall = -1;
-            for (int j = 0; j < n; j++) {
-                if (maze[i][j] == 1) {
-                    wall = j;
-                } else {
-                    leftWalls[i][j] = wall;
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
         System.out.println(new P505_TheMazeII().shortestDistance(new int[][]{
--- src/com/leetcode/P96_UniqueBinarySearchTrees.java
+++ src/com/leetcode/P96_UniqueBinarySearchTrees.java
@@ -14,53 +14,27 @@ public class P96_UniqueBinarySearchTrees {
     private long[] visit;
 
     public int numTrees(int n) {
-        if (visit == null) {
-            visit = new long[n + 1];
-        }
-        if (visit[n] > 0) {
-            return (int)visit[n];
-        }
-        return (int)(visit[n] = dfs((1 << n) - 1));
+        visit = new long[n + 1];
+        visit[0] = 1;
+        visit[1] = 1;
+
+        return (int) count(n);
     }
 
-    private long dfs(long mask) {
-        long result = 0;
-        if (mask == 0) {
-            return 1;
+    private long count(int n) {
+        if (visit[n] != 0) return visit[n];
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += count(i) * count(n - i - 1);
         }
-
-        long left = 0;
-        long right = 0;
-        for (int i = 1; i <= 63; i++) {
-            if ((1 << (i - 1)) > mask) {
-                break;
-            }
-
-            if (((1 << (i - 1)) & mask) > 0 ) {
-                right = 0;
-                for (int j = i + 1; j <= 63; j++) {
-                    if ((1 << (j - 1)) > mask) {break;}
-                    if (((1 << (j - 1)) & mask) > 0 ) {
-                        right ++;
-                    }
-                }
-
-                long leftAns = numTrees((int)left);
-                long rightAns = numTrees((int)right);
-                result += leftAns * rightAns;
-
-                left ++;
-            }
-        }
-
-        return result;
+        return visit[n] = ans;
     }
+
 
     public static void main(String[] args) {
-        System.out.println(new Date().getTime());
-        int ans = new P96_UniqueBinarySearchTrees().numTrees(30);
+        int ans = new P96_UniqueBinarySearchTrees().numTrees(3);
         System.out.println(ans);
-        System.out.println(new Date().getTime());
     }
 
 }
