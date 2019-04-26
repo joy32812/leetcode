240e3f2177ded6beffa6b8fe113585ea2b9d5587
xiaoyuan <joy32812@qq.com>
2019-04-26 23:34:14 +0800

    P679 24games

--- src/com/leetcode/P679_24Game.java
+++ src/com/leetcode/P679_24Game.java
@@ -1,92 +1,68 @@
 package com.leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by xiaoyuan on 17/09/2017.
  */
 public class P679_24Game {
 
-    public void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
-    public void nextPermutation(int[] nums) {
-        if (nums.length == 1) {
-            return;
-        }
-
-        int pos = -1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < nums[i + 1]) {
-                pos = i;
-            }
-        }
-
-        if (pos == -1) {
-            Arrays.sort(nums);
-            return;
-        }
-
-        int target = pos + 1;
-        int diff = nums[target] - nums[pos];
-        for (int i = pos + 2; i < nums.length; i++) {
-            if (nums[i] - nums[pos] > 0 && nums[i] - nums[pos] < diff) {
-                diff = nums[i] - nums[pos];
-                target = i;
-            }
-        }
-
-        swap(nums, pos, target);
-
-        Arrays.sort(nums, pos + 1, nums.length);
-
-    }
-
     public boolean judgePoint24(int[] nums) {
+        List<Double> vals = new ArrayList<>();
+        for (int d : nums) vals.add(0.0 + d);
 
-        for (int i = 0; i < 24; i++) {
-            Set<Double> ans = getAll(nums, 0, 3);
-            if (ans.contains(24.0)) {return true;}
-
-            nextPermutation(nums);
-        }
-
-
-        return false;
-
+        return solve(vals);
     }
 
-    private Set<Double> getAll(int[] nums, int l, int r) {
-        if (l == r) {
-            Set<Double> tmp = new HashSet<>();
-            tmp.add(nums[r] + 0.0);
-            return tmp;
+    private boolean solve(List<Double> vals) {
+        if (vals.size() == 1) {
+            return Math.abs(vals.get(0) - 24) <= 1e-6;
         }
 
-        Set<Double> ans = new HashSet<>();
-        for (int i = l; i < r; i++) {
-            Set<Double> left = getAll(nums, l, i);
-            Set<Double> right = getAll(nums, i + 1, r);
 
-            for (double x : left) {
-                for (double y : right) {
-                    ans.add(x + y);
-                    ans.add(x - y);
-                    ans.add(x * y);
-                    if (y != 0 ) ans.add(x / y);
+        for (int i = 0; i < vals.size(); i++) {
+            for (int j = 0;j < vals.size(); j++) {
+                if (i == j) continue;
+                double x = vals.get(i);
+                double y = vals.get(j);
+
+                List<Double> other = copyOther(vals, i, j);
+                other.add(x + y);
+                if (solve(other)) return true;
+
+                other = copyOther(vals, i, j);
+                other.add(x - y);
+                if (solve(other)) return true;
+
+                other = copyOther(vals, i, j);
+                other.add(x * y);
+                if (solve(other)) return true;
+
+                if (y != 0) {
+                    other = copyOther(vals, i, j);
+                    other.add(x / y);
+                    if (solve(other)) return true;
                 }
             }
+        }
+        return false;
+    }
+
+    private List<Double> copyOther(List<Double> vals, int i, int j) {
+        List<Double> ans = new ArrayList<>();
+        for (int x = 0; x < vals.size(); x++) {
+            if (x == i || x == j) continue;
+            ans.add(vals.get(x));
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        System.out.println(new P679_24Game().judgePoint24(new int[]{1, 2, 1, 2}) );
-        System.out.println(new P679_24Game().judgePoint24(new int[]{4, 1, 8, 7}) );
+        System.out.println(new P679_24Game().judgePoint24(new int[]{
+                4, 1, 8, 7
+        }));
     }
+
 
 }
