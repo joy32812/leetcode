64cd704b9c25724ff1a6e6e48e6f0a587949f0a1
xiaoyuan <joy32812@qq.com>
2019-05-04 15:21:42 +0800

    P796

---
+++ src/com/leetcode/P796_RotateString.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+public class P796_RotateString {
+
+    public boolean rotateString(String A, String B) {
+        if (A == null && B == null) return true;
+        if (A == null || B == null) return false;
+        if (A.length() != B.length()) return false;
+        if (A.length() == 0) return true;
+
+        for (int i = 0; i < A.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(A.substring(i));
+            sb.append(A.substring(0, i));
+
+            if (B.equals(sb.toString())) return true;
+        }
+
+        return false;
+    }
+
+}
