fab4753bad47c0403532ab9a00c410137b364a9b
xiaoyuan <joy32812@qq.com>
2019-07-25 19:51:08 +0800

    P1128 kotlin

---
+++ src/com/leetcode/P1128_NumberofEquivalentDominoPairs.kt
@@ -0,0 +1,28 @@
+package com.leetcode
+
+
+fun numEquivDominoPairs(dominoes: Array<IntArray>): Int {
+
+    val map = mutableMapOf<String, Int>()
+    for (ia in dominoes) {
+        ia.sort()
+        val key = "" + ia[0] + "_" + ia[1];
+        map.put(key, map.getOrDefault(key, 0) + 1)
+    }
+
+    var ans = 0;
+    for ((_, n) in map) {
+        ans += n * (n - 1) / 2
+    }
+
+    return ans
+}
+
+fun main() {
+    println(numEquivDominoPairs(arrayOf(
+            intArrayOf(1, 2),
+            intArrayOf(2, 1),
+            intArrayOf(3, 4),
+            intArrayOf(5, 6)
+    )))
+}
