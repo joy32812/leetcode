23870bded8256854e411cd44f9274f44d795e109
xiaoyuan <joy32812@qq.com>
2019-08-14 11:48:45 +0800

    P1154 deconstruct

--- src/com/leetcode/P1154_DayoftheYear.kt
+++ src/com/leetcode/P1154_DayoftheYear.kt
@@ -5,11 +5,7 @@ fun dayOfYear(date: String): Int {
 
     fun leapYear(year: Int): Boolean = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
     val days = arrayOf(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-
-    val split = date.split("-")
-    val year = split[0].toInt()
-    val month = split[1].toInt()
-    val day = split[2].toInt()
+    val (year, month, day) = date.split("-").map { it.toInt() }
 
     var ans = 0
     for (i in 1 until month) {
