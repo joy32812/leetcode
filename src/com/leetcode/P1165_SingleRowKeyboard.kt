92d80bdee5f3085fe08023a2ddea98214d0cd5fe
xiaoyuan <joy32812@qq.com>
2019-08-25 15:20:12 +0800

    P1165

--- src/com/leetcode/P1165_SingleRowKeyboard.kt
+++ src/com/leetcode/P1165_SingleRowKeyboard.kt
@@ -4,6 +4,5 @@ import kotlin.math.abs
 
 fun calculateTime(keyboard: String, word: String): Int {
     val posMap = keyboard.withIndex().associate { it.value to it.index }
-    val now = keyboard.substring(0, 1) + word
-    return now.zipWithNext().sumBy { abs(posMap[it.second]!! - posMap[it.first]!!) }
+    return ("" + keyboard[0] + word).zipWithNext().sumBy { abs(posMap[it.second]!! - posMap[it.first]!!) }
 }
